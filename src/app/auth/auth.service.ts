c5e5d0b7a7afef69b3b6280911a20cd0cc080dd1
Crystal Campbell <blankpage@gmail.com>
2022-11-06 17:28:58 -0600

    Update: Auth Service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,45 +2,69 @@ import { Injectable } from '@angular/core';
 import { GoogleAuthProvider } from 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { formatCurrency } from '@angular/common';
+import { auth } from 'firebaseui';
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+interface GoogleAuthResponseData {
+
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(
-    public afAuth: AngularFireAuth // Inject Firebase auth service
-  ) {}
+  constructor(public afAuth: AngularFireAuth, private http: HttpClient) {}
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAIulQ4jlbybQtXiLWu5qUuTgOtG0Td00E',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    );
+  }
 
   loggedInUser = {
-    name: "",
-    email: ""
+    name: '',
+    email: '',
+  };
+
+  onSubmitAuth(authForm: NgForm) {
+    let email = authForm.value.password
+    let password = authForm.value.password
+    authForm.reset();
+    console.log(authForm);
   }
-
-  onSubmitAuth(authForm: NgForm){
-    console.log(authForm)
-  }
-
-
 
   // Sign in with Google
   GoogleAuth() {
     return this.AuthLogin(new GoogleAuthProvider());
   }
-  
+
   // Auth logic to run auth providers
   AuthLogin(provider) {
     return this.afAuth
       .signInWithPopup(provider)
       .then((result) => {
         console.log(result);
-        console.log(this.loggedInUser)
+        console.log(this.loggedInUser);
         console.log('You have been successfully logged in!');
-
       })
       .catch((error) => {
         console.log(error);
       });
   }
-
 
   //logout
 }
