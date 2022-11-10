475307ac322d87d374e77d4ce1ff16b5a55bb532
Crystal Campbell <blankpage@gmail.com>
2022-11-10 11:00:18 -0600

    Update: Issue Service
    Added functions to get, post, and delete issues

--- src/app/shared/issue.service.ts
+++ src/app/shared/issue.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Issue } from './issue.model';
 import { doc, setDoc } from "firebase/firestore";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpEventType,
+  HttpRequest
+} from '@angular/common/http';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { Project } from './project.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -32,8 +44,58 @@ export class IssueService {
     return this.issues.slice();
   }
 
-  
+  postIssues(project: Project){
+
+    this.http.post(
+        'https://it-db-ad530-default-rtdb.firebaseio.com/issues/${projectID}.json',
+        project,
+        {
+          observe: 'response'
+        }
+      )
+      .subscribe(
+        responseData => {
+          console.log(responseData);
+        },
+      );
+
+  }
 
 
-  constructor() { }
+  deleteIssue(issue: Issue, projectID){
+    let alertResp = confirm(`Do you really want to delete ${issue.title}?`)
+    if (alertResp){
+      return this.http.delete(
+        `https://it-db-ad530-default-rtdb.firebaseio.com/Issues/${projectID}.json`,
+        {
+          observe: 'response'
+        }
+      )
+
+    }
+    else{
+      alert(`${issue.title} was saved from the fire. This time.`)
+    }
+  }
+
+
+  onFetchIssues(projectID){
+
+    return this.http.get(`https://it-db-ad530-default-rtdb.firebaseio.com/issues.json?orderBy="Project_ID"&startAt="${projectID}"&endAt="${projectID}"`).pipe(map(respData => {
+      let issueArr = [];
+      for(let key in respData){
+
+        issueArr.push({ ...respData[key], ID: key})
+
+      }
+      console.log(issueArr)
+      return issueArr;
+    }))
+  }
+
+
+
+
+
+  constructor(private http: HttpClient, private authService: AuthService) { }
 }
