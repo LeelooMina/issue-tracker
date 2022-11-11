e02c506661f97440fd4dfb29b4ff14dc58943f81
Crystal Campbell <blankpage@gmail.com>
2022-11-10 20:17:06 -0600

    Update: Subscribe to projectList changes

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -5,46 +5,45 @@ import { ProjectService } from '../shared/project.service';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
+  constructor(private projectService: ProjectService) {}
+  projectList: Project[] = [];
 
-  constructor(private projectService: ProjectService) { }
-  projectList: Project[] = []
+  user = 'blankpage@gmail.com';
 
-  user = "blankpage@gmail.com"
-
-  loadProject(project){
-    console.log(project)
+  loadProject(project) {
+    console.log(project);
   }
 
-  onDelete(project){
-    this.projectService.deleteProject(project).subscribe(
-      responseData => {
-        this.projectService.onFetchProjects(this.user).subscribe((payload)=>{
-          this.projectList = payload
-          })
-        console.log(responseData);
-      },
-    );
+  onDelete(project) {
+    this.projectService.deleteProject(project).subscribe((responseData) => {
+      this.projectService.onFetchProjects(this.user).subscribe((payload) => {
+        this.projectList = payload;
+      });
+      console.log(responseData);
+    });
 
-    alert(`${project.name} is now gone forever.`)
+    alert(`${project.name} is now gone forever.`);
   }
 
   ngOnInit(): void {
     // this.projectList = this.projectService.getProjects();
 
-this.projectService.onFetchProjects(this.user).subscribe((payload)=>{
-this.projectList = payload
-})
+    this.projectService.onFetchProjects(this.user).subscribe((payload) => {
+      this.projectList = payload;
+    });
+
     // Subscribe to that subject here
+    this.projectService.projectSubject.subscribe((projects) => {
+      this.projectList = projects;
+    });
   }
 
-  onClickSearch(){
-
-this.projectService.onFetchProjects(this.user).subscribe((payload)=>{
-  this.projectList = payload
-  })
+  onClickSearch() {
+    this.projectService.onFetchProjects(this.user).subscribe((payload) => {
+      this.projectList = payload;
+    });
   }
-
 }
