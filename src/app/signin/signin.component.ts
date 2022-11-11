69ebc577a6910c9be17c00fbd6981a5ba78993a7
Crystal Campbell <blankpage@gmail.com>
2022-11-10 18:12:57 -0600

    Add: Sign in/out logic

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -38,6 +39,17 @@ export class SignInComponent implements OnInit {
   }
 
 
-  constructor(public authService: AuthService) {}
-  ngOnInit() {}
+  constructor(public authService: AuthService, private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params['id'])
+      if(params['id'] === 'in'){
+        this.isLoginMode = true;
+      }
+      else if(params['id'] === 'up'){
+        this.isLoginMode = false;
+      }
+
+  })
+  }
 }
