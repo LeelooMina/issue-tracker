5db87988482e7064fe72e365fae0297bf7900bad
Crystal Campbell <blankpage@gmail.com>
2022-11-11 19:39:30 -0600

    Add: Issue form elements

--- src/app/projects/add-edit-issues/add-edit-issues.component.ts
+++ src/app/projects/add-edit-issues/add-edit-issues.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Auth } from 'firebase/auth';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { Issue } from 'src/app/shared/issue.model';
 
 @Component({
   selector: 'app-add-edit-issues',
@@ -9,8 +13,23 @@ import { ActivatedRoute } from '@angular/router';
 export class AddEditIssuesComponent implements OnInit {
   ID: string;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private authService: AuthService, public router: Router) { }
 
+  onSubmit(addProject: NgForm){
+    console.log("email", this.authService.loggedInUser.email)
+  let issue: Issue  = {
+    name: addProject.value.projectName,
+  description: addProject.value.projectDes,
+  admin: this.authService.loggedInUser.email,
+  allowedUsers: addProject.value.allowedUsers,
+  ID: 0 }
+
+  this.projectService.postProjects(project);
+  this.router.navigate(['/projects'])
+
+  console.log(project);
+
+}
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.ID = params['id'];
