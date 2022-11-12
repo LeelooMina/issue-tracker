e3092a8a48063c2f443bbd2aaeb61fa727f4b1ca
Crystal Campbell <blankpage@gmail.com>
2022-11-12 00:41:23 -0600

    Refactor: Match Issue model & functions
    Swapping out the word "project" for the word "issue" in code copied from project service to issue service

--- src/app/projects/add-edit-issues/add-edit-issues.component.ts
+++ src/app/projects/add-edit-issues/add-edit-issues.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Auth } from 'firebase/auth';
 import { AuthService } from 'src/app/shared/auth/auth.service';
 import { Issue } from 'src/app/shared/issue.model';
+import { IssueService } from 'src/app/shared/issue.service';
 
 @Component({
   selector: 'app-add-edit-issues',
@@ -13,21 +14,22 @@ import { Issue } from 'src/app/shared/issue.model';
 export class AddEditIssuesComponent implements OnInit {
   ID: string;
 
-  constructor(private route: ActivatedRoute, private authService: AuthService, public router: Router) { }
+  constructor(private route: ActivatedRoute, private authService: AuthService, public router: Router, private issueService: IssueService) { }
 
   onSubmit(addProject: NgForm){
     console.log("email", this.authService.loggedInUser.email)
   let issue: Issue  = {
-    name: addProject.value.projectName,
+    title: addProject.value.projectName,
   description: addProject.value.projectDes,
-  admin: this.authService.loggedInUser.email,
-  allowedUsers: addProject.value.allowedUsers,
+  user: this.authService.loggedInUser.email,
+  type: addProject.value.allowedUsers,
+  projectID: this.ID,
   ID: 0 }
 
-  this.projectService.postProjects(project);
+  this.issueService.postIssues(issue, this.ID);
   this.router.navigate(['/projects'])
 
-  console.log(project);
+  console.log(issue);
 
 }
   ngOnInit(): void {
