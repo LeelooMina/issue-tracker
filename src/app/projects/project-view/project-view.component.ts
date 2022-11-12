8789df81419f367dc23eaf91bd9923d68190c42b
Crystal Campbell <blankpage@gmail.com>
2022-11-11 19:38:09 -0600

    Update: Import project service and model into issue view

--- src/app/projects/project-view/project-view.component.ts
+++ src/app/projects/project-view/project-view.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from 'src/app/shared/project.service';
+import { Project } from 'src/app/shared/project.model';
 import { IssueService } from 'src/app/shared/issue.service';
 import { Issue } from 'src/app/shared/issue.model';
 
@@ -12,15 +14,24 @@ export class ProjectViewComponent implements OnInit {
 
   ID: string;
 
+  projectName: string;
+
   issueList: Issue[] = [];
 
-  constructor(private route: ActivatedRoute, private issueService: IssueService) { }
+  constructor(private route: ActivatedRoute, private issueService: IssueService, private projectService: ProjectService) { }
 
   ngOnInit(): void {
+
     this.route.params.subscribe(params => {
       this.ID = params['id'];
-  })
+      })
+
+  this.route.params.subscribe(params => {
+    this.projectName = params['name'];
+})
   this.issueList = this.issueService.getIssues()
+
+
+  }
 }
 
-}
