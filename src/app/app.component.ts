89b8f5b40f3a29ed40195138ce4038f0cc011d63
Crystal Campbell <blankpage@gmail.com>
2022-12-11 10:21:19 -0600

    Feat: Autologin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+import { AuthService } from './shared/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,17 @@ export class AppComponent {
   title = 'issue-tracker';
   items: Observable<any[]>;
 
-  constructor(firestore: AngularFirestore){
+  constructor(
+    firestore: AngularFirestore,
+    private authService: AuthService){
     this.items = firestore.collection('items').valueChanges();
+
+  }
+
+
+
+  ngOnInit(): void {
+   this.authService.autoLogin()
   }
 }
+
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -37,6 +37,8 @@ export class AuthService {
     email: '',
   };
 
+  private tokenExpirationTimer: any;
+
 
 
   signUp(email: string, password: string) {
@@ -82,11 +84,50 @@ export class AuthService {
       }));
   }
 
+  autoLogin() {
+    const userData: {
+      email: string,
+      id: string,
+      _token: string,
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime()
+      this.autoLogout(expirationDuration);
+    }
+  }
+
+  autoLogout(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
+  }
+
   logout(){
     this.user.next(null);
     this.router.navigate(['/sign/out'])
-
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer);
+    }
+    this.tokenExpirationTimer = null;
+  if (this.tokenExpirationTimer) {
+    clearTimeout(this.tokenExpirationTimer);
   }
+  this.tokenExpirationTimer = null;
+}
+
 
   private handleAuth(
     email: string,
@@ -104,6 +145,8 @@ export class AuthService {
       this.userEmail = user.email;
 
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private errorHandling(errorRes: HttpErrorResponse) {
